9c0e6ff0126ed0747ba5d2f981baa8ce66196abb
Andrew Mituykov <bobr_degit@bk.ru>
2022-05-21 13:29:45 +0300

    init

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat beanCat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getVoice());
+        Cat beanCat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat2.getVoice());
+        boolean comrareHelloWorld = bean == bean2;
+        boolean compareCat = beanCat == beanCat2;
+        System.out.println(comrareHelloWorld);
+        System.out.println(compareCat);
     }
 }
